5893148911d5d2f93bd8ed2a38aa1fe0649814ce
Abhishek Pandit <policyglot@users.noreply.github.com>
2017-11-22 10:41:37 +0530

    Added different clauses  for string addition
    Got the error '10 + 2 + ' should equal '10 + 2'. So basically that addition of the plus sign has to stop. Turns out that will require a conditional class too

--- Integer-Based/expanded_form.py
+++ Integer-Based/expanded_form.py
@@ -13,17 +13,20 @@ def expanded_form(num):
 		#The main challenges here is going to be that integers are not iterables
 		# Normally, I would try iterating on something of the likes of the second suggestion here: https://stackoverflow.com/questions/974952/split-an-integer-into-digits-to-compute-an-isbn-checksum
 		#But since the end output is going to be a string, we might as well string methods
-		string_num= str(num)
-		expanded= ""
-		#We'll keep adding to the empty string as we process each digit
-		for idx, digit in enumerate(string_num, 1):
-			#Taking advice from here on keeping indices while looping: https://stackoverflow.com/questions/522563/accessing-the-index-in-python-for-loops
-			if not digit="0":
-				num_zeros= len(string_num)- idx
-				for _ in itertools.repeat(None, num_zeros):
-					digit+="0"
-    			expanded+= digit+" "
-			else:
-				continue
-		return expanded
+	string_num= str(num)
+	expanded= ""
+	#We'll keep adding to the empty string as we process each digit
+	for idx, digit in enumerate(string_num, 1):
+		# The second argument is to ensure that indexing starts from 1 instead of 0
+		# Taking advice from here on keeping indices while looping: https://stackoverflow.com/questions/522563/accessing-the-index-in-python-for-loops
+		if not digit="0":
+			num_zeros= len(string_num)- idx
+			for _ in itertools.repeat(None, num_zeros):
+				digit+="0"
+			expanded+= digit
+			while idx< len(string_num):
+				expanded+= " + "
+		else:
+			continue
+	return expanded
 
