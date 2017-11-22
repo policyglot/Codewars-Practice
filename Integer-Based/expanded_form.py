72e8fc50c1a65a7bed9171555c52d106c543aa50
Abhishek Pandit <policyglot@users.noreply.github.com>
2017-11-22 20:47:29 +0530

    Appended best practices
    Will go over what they did right and that I could have done better

--- Integer-Based/expanded_form.py
+++ Integer-Based/expanded_form.py
@@ -34,3 +34,40 @@ def expanded_form(num):
 
 	return expanded
 
+
+"""Saw the Better Answers in the Kata
+def expanded_form(n):
+    result = []
+    for a in range(len(str(n)) - 1, -1, -1):
+        current = 10 ** a
+        quo, n = divmod(n, current)
+        if quo:
+            result.append(str(quo * current))
+    return ' + '.join(result)
+
+
+def expanded_form(num):
+    num = str(num)
+    st = ''
+    for j, i in enumerate(num):
+        if i != '0':
+            st += ' + {}{}'.format(i, (len(num[j+1:])*'0'))
+    return st.strip(' +')
+
+
+def expanded_form(num):
+    num = list(str(num))
+    return ' + '.join(x + '0' * (len(num) - y - 1) for y,x in enumerate(num) if x != '0')
+
+
+
+def expanded_form(num):
+    return " + ".join([str(int(d) * 10**p) for p, d in enumerate(str(num)[::-1]) if d != "0"][::-1])
+
+
+
+
+
+
+
+"""
